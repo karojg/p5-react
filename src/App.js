d54b4c039a083a9f3d63997dd5bd098acb9a8484
Karolina Jimenez <karojg@gmail.com>
2020-03-29 13:01:09 -0600

    master: quick p5.js implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import p5 from 'p5';
 
 function App() {
+
+  let sketch = p => {
+    p.setup = () => {
+      p.createCanvas(700, 410);
+      p.background(0);
+      p.stroke(0, 0, 0, 15);
+    };
+
+    p.draw = () => {
+      if (p.mouseIsPressed) {
+        p.stroke(255);
+      } else {
+        p.stroke(237, 34, 93);
+      }
+      p.line(p.mouseX - 66, p.mouseY, p.mouseX + 66, p.mouseY);
+      p.line(p.mouseX, p.mouseY - 66, p.mouseX, p.mouseY + 66);
+    };
+  };
+
+  let sketch2 = p => {
+    p.setup = () => {
+      p.createCanvas(720, 400);
+      p.noStroke();
+      p.rectMode(p.CENTER);
+    };
+
+    p.draw = () => {
+      p.background(230);
+
+      let r1 = p.map(p.mouseX, 0, p.width, 0, p.height);
+      let r2 = p.height - r1;
+
+      p.fill(237, 34, 93, r1);
+      p.rect(p.width / 2 + r1 / 2, p.height / 2, r1, r1);
+
+      p.fill(237, 34, 93, r2);
+      p.rect(p.width / 2 - r2 / 2, p.height / 2, r2, r2);
+    };
+  };
+
+  new p5(sketch);
+  new p5(sketch2);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
